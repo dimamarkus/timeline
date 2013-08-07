7aa127ab74962a9d38d36c22bc061c36d6340e74
Dima Markus <dmi3000@gmail.com>
2013-08-07 01:33:22 -0400

    Initial commit

---
+++ js/plugins-ck.js
@@ -0,0 +1,2 @@
+// Avoid `console` errors in browsers that lack a console.
+(function(){var e,t=function(){},n=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeStamp","trace","warn"],r=n.length,i=window.console=window.console||{};while(r--){e=n[r];i[e]||(i[e]=t)}})();$(function(){$("#flipbox").on("click",function(){$("#flipbox").flip({direction:"tb",content:"<p> hi</p>"})});$("#flipswitch").on("click",function(){$("#flipbox").revertFlip()})});
